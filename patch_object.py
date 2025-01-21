f9c72f92d96f3a29d0d1eee25da99766d4e9980c
kristina <kristi.nochka@bk.ru>
2025-01-21 10:00:19 +0700

    add new files with endpoints

---
+++ patch_object.py
@@ -0,0 +1,11 @@
+import requests
+from endpoints.base_endpoint import Endpoint
+
+class PatchObject(Endpoint):
+
+    def patch_object_by_id(self, object_id, payload):
+        self.response = requests.patch(f'https://api.restful-api.dev/objects/{object_id}', json=payload)
+        self.response_json = self.response.json()
+
+    def check_response_name(self, name):
+        assert self.response_json['name'] == name
