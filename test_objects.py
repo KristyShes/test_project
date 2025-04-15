5b7e209611c534203ab1f03b0d2a96751de8e38f
kristina <kristi.nochka@bk.ru>
2025-04-15 14:25:27 +0700

    addes allure report

--- test_objects.py
+++ test_objects.py
@@ -1,12 +1,17 @@
 import requests
 import pytest
+import allure
+
 from endpoints.create_object import CreateObject
 from endpoints.get_object import GetObject
 from endpoints.update_object import UpdateObject
 from endpoints.delete_object import DeleteObject
 from endpoints.patch_object import PatchObject
+from endpoints.get_all_objects import GetAllObjects
 
 
+@allure.feature('Create object')
+@allure.story('creativity')
 def test_create_object():
     new_object_endpoint = CreateObject()
     payload = {
@@ -22,18 +27,21 @@ def test_create_object():
     new_object_endpoint.check_response_is_200()
     new_object_endpoint.check_name(payload['name'])
 
-
+@allure.feature('Get object')
+@allure.story('getting')
 def test_get_object(obj_id):
     get_obj_endpoint = GetObject()
     get_obj_endpoint.get_by_id(obj_id)
     get_obj_endpoint.check_response_is_200()
     get_obj_endpoint.check_response_id(obj_id)
 
-
+@allure.feature('Get all objects')
+@allure.story('getting')
 def test_get_objects():
-    response = requests.get(f'https://api.restful-api.dev/objects').json()
+    get_all_objects_endpoint = GetAllObjects()
 
-
+@allure.feature('Partition update object')
+@allure.story('update')
 def test_patch_object(obj_id):
     patch_object_endpoint = PatchObject()
     payload = {
@@ -43,7 +51,8 @@ def test_patch_object(obj_id):
     patch_object_endpoint.check_response_is_200()
     patch_object_endpoint.check_response_name(payload['name'])
 
-
+@allure.feature('Update object')
+@allure.story('update')
 def test_update_object(obj_id):
     update_object_endpoint = UpdateObject()
     payload = {
@@ -59,7 +68,8 @@ def test_update_object(obj_id):
     update_object_endpoint.check_response_is_200()
     update_object_endpoint.check_response_name(payload['name'])
 
-
+@allure.feature('Delete object')
+@allure.story('delete')
 def test_delete_object(obj_id):
     delete_obj_endpoint = DeleteObject()
     delete_obj_endpoint.delete_by_id(obj_id)
